13420c06e3f363be6546467853dbbf7937d17a2f
Olha Mushket <olhamushket@gmail.com>
2022-06-28 15:12:07 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -1,12 +1,16 @@
 package com.epam.rd.autotasks.meetautocode;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
-
 public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
-
-
+        int h, m, s;
+        h = seconds/3600%24;
+        m = seconds%3600/60;
+        s = seconds%3600%60;
+        DecimalFormat df = new DecimalFormat("00");
+        System.out.println(h + ":" + df.format(m) + ":" + df.format(s));
     }
 }
